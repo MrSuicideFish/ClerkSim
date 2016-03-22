444934cb08295aa79357b894a64d2b20e972a41d
Malcolm Neal <Malcolm259@Gmail.com>
2016-03-21 21:08:01 -0700

    Added GameInfo class

---
+++ ClerkSim/Assets/Scripts/GameInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameInfo
+{
+    public static string m_PlayerName { get; set; }
+
+    public GameInfo( string playerName )
+    {
+        m_PlayerName = playerName;
+    }
+}
